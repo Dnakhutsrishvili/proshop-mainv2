942dba2367d705d9cbca51ad8265e133df8f28ed
davit <dtnakhutsrishvili@gmail.com>
2024-05-24 16:19:22 +0400

    deployment

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js'
 import {notFound,errorHandler}from './middleware/errorMiddleware.js';
 import cors from "cors";
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const port=3000;
 const app=express();
@@ -39,6 +41,22 @@ app.get('/log-cookies', (req, res) => {
 const __dirname = path.resolve();
 
 app.use('/uploads', express.static(path.join(`${__dirname}/frontend/public`, 'uploads')));
+
+
+if(process.env.NODE_ENV === 'production'){
+
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
+  })
+}{
+  app.get('/',(req,res)=>{
+    res.send('api running...')
+  })
+
+}
+
 
 app.use(notFound);
 app.use(errorHandler);
